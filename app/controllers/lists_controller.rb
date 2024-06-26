2892eb979277f0ef8485556ce91d9cb2447bdbfb
Amelie Caron <caron.amelie94@gmail.com>
2024-06-26 11:08:29 +0200

    exo en cours

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,11 +7,22 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
-  # def create
-  # end
+  def new
+    @list = List.new
+  end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  # def new
-  # end
+private
 
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "lists#index"
-
-  resources :lists, only: [:index, :show, :create, :new] do
-    resources :bookmarks, only: [:create, :new]
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
   end
-  resources :bookmarks, only: [:destroy]
+
+  resources :bookmarks, only: :destroy
+  # Defines the root path route ("/")
+  root 'lists#index'
 end
